ce4a89645ed194857408cc706d4afa16042a3c36
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-07-30 23:09:38 +0530

    iterative assigmnets

---
+++ iterative statements/asssigments/24factorswhile.js
@@ -0,0 +1,7 @@
+let i = 1;
+while (i<=24) {
+    if (24 % i == 0) {
+        console.log(i)
+    }
+    i++
+}
---
+++ iterative statements/asssigments/6multiplewhile.js
@@ -0,0 +1,6 @@
+let i=1;
+while (i<=10) {
+    // console.log(6*i)
+    console.log("6*",i,"=",6*i)
+    i++
+}
