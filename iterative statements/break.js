9a8239ca9ca90de03e44206a665717ec7f30a450
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-07-31 22:38:47 +0530

    statemets

---
+++ iterative statements/break.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <=100 ; i++) {
+    if (i==12) {
+        break;
+    }
+    console.log(i)    
+}
---
+++ iterative statements/continue.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <=100 ; i++) {
+    if (i>=67 && i<=84) {
+        continue;
+    }
+    console.log(i)    
+}
