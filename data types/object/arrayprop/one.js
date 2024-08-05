57bbf506153935e09afef433f750031d24110a83
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-08-05 13:35:49 +0530

    array methods

---
+++ data types/object/arrayprop/one.js
@@ -0,0 +1,13 @@
+let arr=[10,20.5,"rahul",true,{},[],null,10]
+let a=[10,20,30,40]
+// console.log(arr)
+a.push(50)// adding at the last of the array
+console.log(a)
+a.unshift(5)
+console.log(a)//addimg element at the starting of the arrray
+console.log(a)
+a.pop()
+console.log(a)//deleting at the last of the array
+console.log(a)
+a.shift()
+console.log(a)//removing element at the starting of the arrray
