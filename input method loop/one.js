e3ff4b8ce8c83ae9de55c3ded785f09d79fe4bb7
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-09-03 16:04:33 +0530

    iterative assigments

--- input method loop/one.js
+++ input method loop/one.js
@@ -1,3 +1,18 @@
-const input= require ('prompt-sync')();
-x=input("enter the number:")
-console.log(x)
+const input = require('prompt-sync')();
+n=parseInt(input("enter the array size:"))
+array = []
+for (i = 0; i < n; i++) {
+    x = input("enter the number:")
+    array.push(x)
+}
+console.log(array)
+console.log(array.length)
+even=[]
+i = 0
+while (i < array.length) {
+    if(array[i]%2==0){
+    // console.log(array[i])}
+    even.push(array[i])}
+    i=i+1
+}
+console.log(" even numbers are: ",even)
