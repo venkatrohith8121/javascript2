dac9ea19d883580338d4c274501216f458dc7f8c
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-10-28 23:13:29 +0530

    28/10/2024

--- ing2str/data.js
+++ ing2str/data.js
@@ -1,3 +1,14 @@
+var reader;
 let img2str=()=>{
 
+    let imagefile=document.getElementById('img_file').files[0];
+    reader=new FileReader();
+    reader.readAsDataURL(imagefile)
+    reader.addEventListener("load",()=>{
+        document.getElementById('str_tag').innerHTML=reader.result;
+        });
+};
+let str2img=()=>{
+    console.log(reader.result)
+    document.getElementById('img_tag').src=reader.result;
 }
